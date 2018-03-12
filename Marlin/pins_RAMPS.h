d6208b759eeeba11e8975e81d9084bec78a638e0
Jeremy Gold <jeremy.gold@ikegps.com>
2018-03-12 22:30:12 +1300

    Initial bringup - Basic X and Y operational

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -93,6 +93,18 @@
 #define X_ENABLE_PIN       38
 #define X_CS_PIN           53
 
+// JG 12 Mar 18 - Swapping Z and Y to use double connector on RAMPS board
+#define Y_STEP_PIN         46
+#define Y_DIR_PIN          48
+#define Y_ENABLE_PIN       62
+#define Y_CS_PIN           40
+
+#define Z_STEP_PIN         60
+#define Z_DIR_PIN          61
+#define Z_ENABLE_PIN       56
+#define Z_CS_PIN           49
+
+/*
 #define Y_STEP_PIN         60
 #define Y_DIR_PIN          61
 #define Y_ENABLE_PIN       56
@@ -102,6 +114,7 @@
 #define Z_DIR_PIN          48
 #define Z_ENABLE_PIN       62
 #define Z_CS_PIN           40
+*/
 
 #define E0_STEP_PIN        26
 #define E0_DIR_PIN         28
